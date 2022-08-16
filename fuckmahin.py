3b3c6fd059ec5f6b1f21874c44f4375809ab6b76
Nahid Afridy <85471971+NAHID-AFRIDY@users.noreply.github.com>
2022-08-16 23:02:57 +0600

    Update fuckmahin.py

--- fuckmahin.py
+++ fuckmahin.py
@@ -1,11 +1,1 @@
-import os, platform
-os.system('git pull')
-
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    import a
-    menu()
-elif bit == '32bit':
-    import a
-    menu()
+import a
